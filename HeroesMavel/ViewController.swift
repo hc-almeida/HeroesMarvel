a61d0e916ab971333a1e2036c09b753175af0a4e
Hellen Caroline <azev.hellen@gmail.com>
2021-10-18 21:57:06 -0300

    Initial settings

---
+++ HeroesMavel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HeroesMavel
+//
+//  Created by Hellen on 18/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
